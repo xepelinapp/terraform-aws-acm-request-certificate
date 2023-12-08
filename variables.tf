92c4839980dfd981041a727fadf2dada4259d3c1
Sebastian Rivera <sebastian.rivera@xepelin.com>
2023-12-08 10:55:48 -0600

    Add tags and region

--- variables.tf
+++ variables.tf
@@ -66,3 +66,42 @@ variable "certificate_authority_arn" {
   default     = null
   description = "ARN of an ACM PCA"
 }
+
+variable "tags" {
+  type = object({
+    name        = string
+    owner       = string
+    environment = string
+    team        = string
+    repo        = string
+    provision   = string
+    ticket      = string
+  })
+  description = <<-EOT
+  name:
+    Default name for resources if it was not provided at resource-level.
+
+  owner:
+    Functional team accountable for resources created.
+
+  environment:
+    Environment resources are being created at.
+    Example values: sandbox, dev, qa, prod, homo
+
+  team:
+    team to which resources created correspond to.
+
+  repo:
+    Github repository
+  
+  provision:
+    Provision method. (terraform)
+  
+  ticket:
+    Jira ticket link. (N/A default)
+  EOT
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
